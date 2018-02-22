836f43670adfac5c9395a37d403ba5d17dae7045
petrofang <gilescooper@gmail.com>
2018-02-22 11:38:47 -0500

    Fill in the underlying code to create the raw score
    Each correct answer is +1 point; each wrong answer is -1 point.
    I found that radio buttons were easier to code, but they are a linear
    layout, so the layout options are limited.

--- app/src/main/java/com/example/android/pizzalinequiz/MainActivity.java
+++ app/src/main/java/com/example/android/pizzalinequiz/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.android.pizzalinequiz;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +16,62 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /*
-     * TODO: (2) Create code to handle the scoring, based on the answers.
-     */
+    public void submitScore(View view) {
+        int rawScore = 0;
+
+        /* this is kind of a lot, scoring one-bye-one. I feel like there's an easier way... */
+        CheckBox checkBox = findViewById(R.id.checkBox1a);
+        if (checkBox.isChecked()) rawScore++;
+        else rawScore--;
+        checkBox = findViewById(R.id.checkBox1b);
+        if (checkBox.isChecked()) rawScore--;
+        checkBox = findViewById(R.id.checkBox1c);
+        if (checkBox.isChecked()) rawScore--;
+        checkBox = findViewById(R.id.checkBox1d);
+        if (checkBox.isChecked()) rawScore++;
+        else rawScore--;
+
+        checkBox = findViewById(R.id.checkBox2a);
+        if (checkBox.isChecked()) rawScore--;
+        checkBox = findViewById(R.id.checkBox2b);
+        if (checkBox.isChecked()) rawScore++;
+        else rawScore--;
+        checkBox = findViewById(R.id.checkBox2c);
+        if (checkBox.isChecked()) rawScore--;
+        checkBox = findViewById(R.id.checkBox2d);
+        if (checkBox.isChecked()) rawScore++;
+        else rawScore--;
+
+        RadioButton radioButton = findViewById(R.id.radio3);
+        if (radioButton.isChecked()) rawScore++;
+        else rawScore--;
+        radioButton = findViewById(R.id.radio4);
+        if (radioButton.isChecked()) rawScore++;
+        else rawScore--;
+        radioButton = findViewById(R.id.radio5);
+        if (radioButton.isChecked()) rawScore++;
+        else rawScore--;
+
+        checkBox = findViewById(R.id.checkBox6a);
+        if (checkBox.isChecked()) rawScore++;
+        else rawScore--;
+        checkBox = findViewById(R.id.checkBox6b);
+        if (checkBox.isChecked()) rawScore++;
+        else rawScore--;
+        checkBox = findViewById(R.id.checkBox6c);
+        if (checkBox.isChecked()) rawScore++;
+        else rawScore--;
+        checkBox = findViewById(R.id.checkBox6d);
+        if (checkBox.isChecked()) rawScore++;
+        else rawScore--;
+
+        String answer7 = getResources().getString(R.string.answer7);
+        TextView textView = findViewById(R.id.etAnswer7);
+        String etAnswer7 = textView.getText().toString();
+        if (answer7.equals(etAnswer7)) rawScore++;
+        else rawScore--;
+
+        Toast.makeText(this, "raw score: " + rawScore, Toast.LENGTH_SHORT).show();
+    }
 
 }
