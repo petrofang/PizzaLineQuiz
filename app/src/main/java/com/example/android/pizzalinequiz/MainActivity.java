7a6201f2cbd445a24899c9f9386acb7b9335eb17
petrofang <gilescooper@gmail.com>
2018-02-21 23:18:38 -0500

    Complete a rough layout including RadioButton, CheckBox, and EditText
    I have built the basic framework for the app. I wish I had found a
    more modular way of doing it, because it involved a lot of copy and
    pasting and changing one character per line which felt pretty cheap.
    Also, I can't easily move questions around in order, because they are
    named for their number. Perhaps a better way of doing this would have
    been to use an array or something, or just build the objects in Java.
    
    Next I have to copy the the questions and answers from my paper notes,
    then write the java code.

--- app/src/main/java/com/example/android/pizzalinequiz/MainActivity.java
+++ app/src/main/java/com/example/android/pizzalinequiz/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.android.pizzalinequiz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /*
+     * TODO: (2) Create code to handle the scoring, based on the answers.
+     */
+
 }
